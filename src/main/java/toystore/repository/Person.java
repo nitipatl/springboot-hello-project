042208d0227d5b390a9680e1df988603c2e6ae65
Nitipat L <iamsvz@gmail.com>
2018-03-07 14:51:09 +0700

    add init data

--- src/main/java/toystore/repository/Person.java
+++ src/main/java/toystore/repository/Person.java
@@ -5,7 +5,7 @@ import org.springframework.data.annotation.Id;
 
 public class Person {
     @Id
-    private int id;
+    private String id;
     private String firstName;
     private String lastName;
 
@@ -14,13 +14,10 @@ public class Person {
         this.lastName = lastName;
     }
 
-    public int getId() {
+    public String getId() {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
 
     public String getFirstName() {
         return firstName;
